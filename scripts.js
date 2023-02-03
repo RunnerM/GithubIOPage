bb0fb09b6c58ad337a1cf76ecce83f38fa19231b
RunnerM <51089137+RunnerM@users.noreply.github.com>
2023-02-03 17:37:13 +0000

    Deploying to gh-pages from  @ 1c9e32517a51d9f6f6494a04e4e3ef174dcf48e9 🚀

---
+++ scripts.js
@@ -0,0 +1,11 @@
+﻿// get user preferred color scheme
+window.isDarkModePreferred = () => { return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches; };
+
+// get user saved theme
+window.getSavedTheme = () => { return localStorage.getItem('theme'); };
+
+// set user saved theme
+window.setSavedTheme = (theme) => { localStorage.setItem('theme', theme); };
+
+// as property of window object
+window.theme = () => window.getSavedTheme() || (window.isDarkModePreferred() ? 'dark' : 'light');
